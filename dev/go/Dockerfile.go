85c057911d5ec4c0aee9a8658452081ac77fbe87
Robert Heuts <robert.heuts@popagency.com>
2017-05-09 11:41:09 -0700

    Added Ubunut, Centos and Go environments

---
+++ dev/go/Dockerfile.go
@@ -0,0 +1,9 @@
+FROM amazonlinux:latest
+
+ENV GO_VERSION 1.8.1
+
+RUN yum -y update && yum -y install wget && yum clean all
+RUN wget https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz && \
+  tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+
+ENV PATH ${PATH}:/usr/local/go/bin
