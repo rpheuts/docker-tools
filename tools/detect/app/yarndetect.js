8d568ef8c64c74fd1fda75d51e0df952b6adebbe
Robert Heuts <heutsr@amazon.com>
2023-01-19 14:45:13 -0800

    Removed detect stuff

--- tools/detect/app/yarndetect.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    detect: function (root, fileStat, readFile) {
-        if (fileStat.name === 'yarn.lock') {
-            console.log('> Detected Yarn');
-
-            return true;
-        }
-    }
-};
