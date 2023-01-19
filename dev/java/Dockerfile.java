b465367e16d609a48bae0160952a0d2295238230
Robert Heuts <heutsr@amazon.com>
2023-01-19 12:36:29 -0800

    Switched to Ubuntu as a base image

--- dev/java/Dockerfile.java
+++ dev/java/Dockerfile.java
@@ -1,17 +1,4 @@
-FROM amazonlinux:latest
+FROM ubuntu:latest
 
-ENV JAVA_VERSION 8u171
-ENV BUILD_VERSION b11
- 
 # Upgrading system
-RUN yum -y upgrade
-RUN yum -y install wget which
- 
-# Downloading & Config Java 8
-RUN wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION}-${BUILD_VERSION}/512cd62ec5174c3487ac17c61aaa89e8/jdk-${JAVA_VERSION}-linux-x64.rpm" -O /tmp/jdk-8-linux-x64.rpm
-RUN yum -y install /tmp/jdk-8-linux-x64.rpm
-RUN alternatives --install /usr/bin/java jar /usr/java/latest/bin/java 200000
-RUN alternatives --install /usr/bin/javaws javaws /usr/java/latest/bin/javaws 200000
-RUN alternatives --install /usr/bin/javac javac /usr/java/latest/bin/javac 200000
-
-ENV JAVA_HOME /usr/java/latest/jre
+RUN apt update && apt -y install wget unzip default-jdk && apt autoclean
